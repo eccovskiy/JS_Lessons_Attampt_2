addb7fd7fc6a976395359f4c41b2271d1c3566bc
eccovskiy <eccovskiy@gmail.com>
2020-10-30 20:01:03 +0200

    update 30.10.2020

--- js/script.js
+++ js/script.js
@@ -552,3 +552,52 @@ Object.setPrototypeOf(john, soldier);
 // console.log(john.armor);
 
 john.sayHello();*/
+
+// #25. Отлавливаем ошибки в своём коде при момощи консоли разработчика
+
+// №26. Динамическая типизация в JS
+
+// To string
+// 1)
+console.log(typeof(String(null)));
+
+// 2)
+const num = 5;
+
+console.log("https://vk.com/canalog/" + num);
+
+const fontSize = 26 + 'px';
+
+console.log(fontSize);
+
+// To number
+// 1)
+console.log(typeof(Number(null)));
+
+// 2)
+console.log(typeof(+'5'));
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+let answ = +prompt('Hello', '');
+
+// To boolean
+// False - 0. '', null, undefined, NaN;
+
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+	console.log('Workimg...');
+}
+
+switcher = 1;
+
+if (switcher) {
+	console.log('Workimg...');
+}
+
+// 2)
+console.log(typeof(Boolean(null)));
