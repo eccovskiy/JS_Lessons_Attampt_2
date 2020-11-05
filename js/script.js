c8a2d997c56ad96069233203b2fbd7c8b8966596
eccovskiy <eccovskiy@gmail.com>
2020-11-05 21:29:56 +0200

    update script.js 05.11.2020

--- js/script.js
+++ js/script.js
@@ -559,7 +559,7 @@ john.sayHello();*/
 
 // To string
 // 1)
-console.log(typeof(String(null)));
+/*console.log(typeof(String(null)));
 
 // 2)
 const num = 5;
@@ -601,3 +601,66 @@ if (switcher) {
 
 // 2)
 console.log(typeof(Boolean(null)));
+
+// 3)
+*/
+
+// Задачи для СОБЕСЕДОВАНИЯ и НА ПОНИМАНИЕ ОСНОВ
+
+// 1) Какое будет выведено значение: let x = 5; alert( x++ ); ?
+// let x = 5;
+
+// alert( x++ ); // 5
+
+// 2) Чему равно такое выражение: [ ] + false - null + true ?
+
+// console.log([] + false - null + true); // NaN
+
+// 3) Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
+
+// let y = 1;
+// let x = y = 2;
+
+// alert(x); // 2
+
+// 4) Чему равна сумма [ ] + 1 + 2?
+
+// console.log([] + 1 + 2); // 12
+
+// 5) Что выведет этот код: alert( '1'[0] )?
+
+// alert('1'[0]); // 1
+
+// 6) Чему равно 2 && 1 && null && 0 && undefined ?
+
+// console.log(2 && 1 && null && 0 && undefined); // null
+
+// 7) Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+
+// let a = 5,
+// 	b = 10;
+
+// console.log(!!(a && b) === (a && b)); // false
+
+// 8) Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
+
+// alert( null || 2 && 3 || 4 ); // 3
+
+// 9) a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+
+// let a = [1, 2, 3],
+// 	b = [1, 2, 3];
+
+// console.log(a == b); false
+
+// 10) Что выведет этот код: alert(+'Infinity'); ?
+
+//alert('Infinity'); // Infinity
+
+// 11) Верно ли сравнение: "Ёжик" > "яблоко"?
+
+// console.log("яблоко" > "Ёжик"); // false
+
+// 12) Чему равно 0 || "" || 2 || undefined || true || falsе ?
+
+//console.log(0 || "" || 2 || undefined || true || falsе); // 2
